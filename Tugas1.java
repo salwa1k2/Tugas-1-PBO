00ef2421680f89a87bcd5f6f49a5e58c0eb84861
SalwaKhairunnisa <149360503+salwa1k2@users.noreply.github.com>
2025-02-10 18:11:55 +0700

    Add files via upload

---
+++ Tugas1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tugas1;
+
+/**
+ *
+ * @author Acer
+ */
+public class Tugas1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Salwa Khairunnisa 2304617");
+        System.out.println("Saya Senang Belajar Java");
+        
+    }
+}
